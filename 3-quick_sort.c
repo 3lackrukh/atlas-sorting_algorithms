2eb49d6435b21473e531baf75ff12b1518467074
3lackrukh <nathan.rhys@atlasschool.com>
2024-04-09 03:20:00 +0000

    quick_sort compares the i position and pivot. if right is greater switches i and k, increments i. if not increments k and swaps i and pivot.

---
+++ 3-quick_sort.c
@@ -0,0 +1,37 @@
+#include "sort.h"
+
+/**
+ * quick_sort- sorts an array of integers in ascending order,
+ * printing the after every swap.
+ *
+ * @array: pointer to the array to be sorted.
+ *
+ * @size: the size of the array.
+ *
+ * Return: nothing.
+ */
+
+void quick_sort(int *array, size_t size)
+{
+	size_t i = 0, k = 0;
+	int s, pivot;
+
+	pivot = array[(size - 1)];
+	while (k < (size - 1))
+	{
+		if(array[k] < pivot)
+		{
+			s = array[i];
+			array[i] = array[k];
+			array[k] = s;
+			print_array(array);
+			i++
+		}
+		k++;
+	}
+	s = array[i];
+	array [i] = pivot;
+	pivot = s;
+	print_array(array);
+}
+
